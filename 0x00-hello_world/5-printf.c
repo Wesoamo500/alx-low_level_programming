0727c1cb6c4209165774d24436ef5d5fd78ef66d
Wesoamo500 <sulemanabdulmanan@gmail.com>
2023-06-15 10:58:42 -0700

    code update

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - prints the phrase with proper grammar, but the outcome is a piece of art,
+ * main - prints a phrase 
  * Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
