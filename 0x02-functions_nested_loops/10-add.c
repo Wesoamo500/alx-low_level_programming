e74dc97ff31e3cb3dec365dcd158294f917f6bb6
Wesoamo500 <sulemanabdulmanan@gmail.com>
2023-06-20 06:33:54 -0700

    bug fix

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,12 +2,12 @@
 
 /**
  * add - adds two integers and returns the result
- * @a: int to be added to b
- * @b: int to be added to a
+ * @i: int to be added to b
+ * @j: int to be added to a
  * Return: sum of a and b
  */
 
 int add(int i, int j)
 {
-return (i+j);
+return (i + j);
 }
