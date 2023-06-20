60e3f79a069eb457dfad325deb2d80801cefea7c
Wesoamo500 <sulemanabdulmanan@gmail.com>
2023-06-20 03:48:09 -0700

    add repo and a c-program

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - function prints _putchar
+ * Return : Always 0 (successful)
+ */
+int main(void)
+{
+printf("_putchar\n");
+return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+This directory consists of c programs that perform specific tasks.
+0-putchar :  prints "_putchar"
