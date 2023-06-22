cbd4a4925b3d033dffd69e065d53e8d8bed3adde
Wesoamo500 <sulemanabdulmanan@gmail.com>
2023-06-22 05:45:20 -0700

    fix bug

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,11 +6,13 @@
  */
 void print_most_numbers(void)
 {
-int i = 0;
-while (i < 10 && (i != 2 && i != 4))
+int i;
+for (i = 0; i < 10; i++)
+{
+if (i != 2 && i != 4)
 {
 _putchar(i + '0');
-i++;
+}
 }
 _putchar('\n');
 }
