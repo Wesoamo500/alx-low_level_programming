81c2e55da6f40716890a0704369ef9127d4f5bf3
Wesoamo500 <sulemanabdulmanan@gmail.com>
2023-06-22 05:53:11 -0700

    task 7: fix bug

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -6,16 +6,16 @@
  */
 void print_line(int n)
 {
-int i;
 if (n <= 0)
+{
 _putchar('\n');
+}
 else
 {
-int i = 1;
-while (i != n)
+int i;
+for (i = 1; i <= n; i++)
 {
 _putchar('_');
-i++;
 }
 _putchar('\n');
 }
