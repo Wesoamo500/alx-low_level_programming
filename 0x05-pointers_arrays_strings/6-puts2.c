83fa880570b2617352c7cc38a1c29081f94b37ec
Wesoamo500 <sulemanabdulmanan@gmail.com>
2023-06-26 10:46:32 -0700

    task 7: bugFix

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,21 +1,21 @@
  #include "main.h"
 
 /**
- * print_rev - prints a string in reverse, followed by a new line
- * @s: string to be printed
+ * puts2 - prints a string in reverse, followed by a new line
+ * @str: string to be printed
  */
-void print_rev(char *s)
+void puts2(char *str)
 {
-int i, j, strlen;
+int i, j;
 i = 0;
-while (s[i] != '\0')
+while (str[i] != '\0')
 {
 i++;
 }
 j = 0;
 while (j < i)
 {
-_putchar(s[j]);
+_putchar(str[j]);
 j += 2;
 }
 _putchar('\n');
