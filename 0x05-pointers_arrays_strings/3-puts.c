94d6724b2c2b510dd1650dcad1bf64a922143437
Wesoamo500 <sulemanabdulmanan@gmail.com>
2023-06-26 09:59:11 -0700

    task 4: puts str to stdout

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string to stdout
+ * @str: pointer to the string to print
+ */
+void _put(char *str)
+{
+int i;
+i = 0;
+while (str[i] != '\0')
+{
+_putchar(str[i]);
+i++;
+}
+_putchar('\n');
+}
