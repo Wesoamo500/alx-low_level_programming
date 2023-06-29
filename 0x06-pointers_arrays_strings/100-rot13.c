be965b166cbfca8a838c329feff5b1f9f65400ec
Wesoamo500 <sulemanabdulmanan@gmail.com>
2023-06-29 07:29:02 -0700

    update 100-rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,24 +1,26 @@
 #include "main.h"
 
 /**
- * rot13 - Encodes a string using the ROT13 algorithm.
- * @s: The string to be encoded.
+ * rot13 - encodes a string in rot13
+ * @s: string to be encoded
  *
- * Return: The resulting encoded string.
+ * Return: the resulting string
  */
 char *rot13(char *s)
 {
-int i;
-char *input = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-char *output = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+int i, j;
+char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 for (i = 0; s[i] != '\0'; i++)
 {
-char *ptr = strchr(input, s[i]);
-if (ptr != NULL)
+for (j = 0; a[j] != '\0'; j++)
 {
-int index = ptr - input;
-s[i] = output[index];
+if (s[i] == a[j])
+{
+s[i] = b[j];
+break;
 }
 }
-return s;
+}
+return (s);
 }
