332187513ea492ec4d4c67783475d60089aee09f
Wesoamo500 <sulemanabdulmanan@gmail.com>
2023-07-03 09:49:28 -0700

    task 8: print chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_chessboard - prints the chessboard
+ * @a: two dimension array to print
+ */
+void print_chessboard(char (*a)[8])
+{
+int i = 0, j;
+while (i < 8)
+{
+j = 0;
+while (j < 8)
+{
+_putchar(a[i][j]);
+j++;
+}
+_putchar('\n');
+i++;
+}
+}
