673a9aea581c19c85a26c0722bdccc71177f0eb7
Wesoamo500 <sulemanabdulmanan@gmail.com>
2023-07-13 05:27:34 -0700

    fixbug

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -12,7 +12,8 @@
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 char *p;
-unsigned int i, n = new_size;
+unsigned int i;
+unsigned int n = new_size;
 char *oldp = ptr;
 if (ptr == NULL)
 {
