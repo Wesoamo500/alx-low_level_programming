b0da60af1d3448cad4d12b78ed809a18507389ae
Wesoamo500 <sulemanabdulmanan@gmail.com>
2023-07-14 04:41:49 -0700

    advance task 2

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -9,8 +9,7 @@
 int main(int argc, char *argv[])
 {
 unsigned long mul;
-int i;
-int j;
+int i, j;
 if (argc != 3)
 {
 printf("Error\n");
@@ -27,7 +26,7 @@ exit(98);
 }
 }
 }
-mul = atol(argv[1]) *atol(argv[2]);
+mul = atol(argv[1]) *  atol(argv[2]);
 printf("%lu\n", mul);
 return (0);
 }
