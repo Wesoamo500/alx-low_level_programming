bd685c0c59735683664d53db87a705a0fc218b80
Wesoamo500 <sulemanabdulmanan@gmail.com>
2023-07-17 05:09:44 -0700

    update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,16 +16,13 @@ struct dog
 	char *owner;
 };
 
-/**
- * dog_type - typedef for struct dog
- */
-typedef struct dog dog_type;
-
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+
+typedef struct dog dog_t;
+
 dog_t *new_dog(char *name, float age, char *owner);
+
 void free_dog(dog_t *d);
-char *_strcpy(char *dest, char *src);
-int _strlen(char *s);
 
 #endif
