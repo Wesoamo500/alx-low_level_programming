62879eddb840b803b8e37be2b5d4907d6954be05
Wesoamo500 <sulemanabdulmanan@gmail.com>
2023-08-03 17:35:26 -0700

    bugfix

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -9,12 +9,12 @@
 int clear_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int i;
-	unsigned int hld;
+	unsigned int hold;
 
 	if (index > 64)
 		return (-1);
 	hold = index;
-	for (i = 1; hld > 0; i *= 2, hld--)
+	for (i = 1; hold > 0; i *= 2, hold--)
 		;
 
 	if ((*n >> index) & 1)
